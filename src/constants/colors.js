c79e445f161290849e688a14f1666103288c74b2
Cosimo Chetta <minochetta@gmail.com>
2022-01-21 01:22:00 +0100

    feat: daily graph with moving average

--- src/constants/colors.js
+++ src/constants/colors.js
@@ -3,6 +3,9 @@ export const DEATHS = "rgb(255, 153, 153)";
 export const VACCINATED = "rgba(153, 255, 255)"
 export const PARTIALLY_VACCINATED = "rgb(200,255,153)"
 export const NON_VACCINATED = "rgba(120, 120, 120)"
-export const CONFIRMED_BG = "rgba(255,255,102, 0.4)";
-export const DEATHS_BG = "rgba(255, 102, 102, 0.4)";
+export const CONFIRMED_BAR = "rgb(238, 238, 155)";
+export const CONFIRMED_LINE = "rgb(255, 204, 0)";
+export const DEATH_LINE = "rgb(255, 0, 0)";
+export const DEATH_BAR = "rgb(238, 153, 153)";
+
 export const COUNTRY = "rgb(102,102,102)";
--- src/constants/titles.js
+++ src/constants/titles.js
@@ -1,6 +1,6 @@
 export const SUMMARY = "Summary";
 export const VACCINATION = "Vaccination Percentage";
-export const DAILY_CONFIRMED = "Daily Case";
+export const DAILY_CONFIRMED = "Daily Cases";
 export const DAILY_DEATHS = "Daily Deaths";
 export const TOTAL_CONFIRMED = "Total Cases";
 export const TOTAL_DEATHS = "Total Deaths";
